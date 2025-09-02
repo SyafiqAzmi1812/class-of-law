51da93b36c8b04f9ffdfe1b11f8aa1fe95c6c1f5
Tiery <tieryumar@gmail.com>
2025-09-02 11:03:52 +0700

    Awal

---
+++ home.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda | Class Of Law - Website Hukum KUHP</title>
+</head>
+<body>
+    <nav>
+        <div class="kiri">
+            <div class="logo">
+                <a href="home.php" id="logo_link">
+                    <img src="./img/logo/logo.png" alt="Class Of Law" id="logo_img">
+                </a>
+            </div>
+            <div class="menu_list">
+                <a href="#" id="menu">Beranda</a>
+                <a href="#" id="menu">Perpustakaan</a>
+                <a href="#" id="menu">Kuis</a>
+                <a href="#" id="menu">Umpan Balik</a>
+            </div>
+        </div>
+        <div class="kanan">
+            <div class="search">
+                <input type="search" name="search" id="search_bar" placeholder="Search">
+                <button id="btn_search"></button>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
