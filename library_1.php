bcfc06195de75c7c6814a4f1944cae9fc1153a4f
Raihan Ghani Fausta <ghanifaustaraihan@gmail.com>
2025-09-03 08:38:56 +0700

    apa da cuman nambah navbar di library_1

--- library_1.php
+++ library_1.php
@@ -5,10 +5,32 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Library Buku 1 KUHP</title>
+
+  <link rel="stylesheet" href="style/home.css">
 </head>
 
 <body>
-  
+  <nav>
+    <div class="kiri">
+      <div class="logo">
+        <a href="home.php" id="logo_link">
+          <img src="./img/logo/logo.png" alt="Class Of Law" id="logo_img">
+        </a>
+      </div>
+      <div class="menu_list">
+        <a href="#" id="menu">Beranda</a>
+        <a href="#" id="menu">Perpustakaan</a>
+        <a href="#" id="menu">Kuis</a>
+        <a href="#" id="menu">Umpan Balik</a>
+      </div>
+    </div>
+    <div class="kanan">
+      <div class="search">
+        <input type="search" name="search" id="search_bar" placeholder="Search">
+        <button id="btn_search"></button>
+      </div>
+    </div>
+  </nav>
 </body>
 
 </html>
